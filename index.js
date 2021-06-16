ff678fdcb853d552f38c6e94b1976429fc892cd6
ewelina-support <85874932+ewelina-support@users.noreply.github.com>
2021-06-16 16:03:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,12 +38,18 @@ app.use(
 );
 
 app.use(
-   auth({
-      secret: SESSION_SECRET,
-      authRequired: false, // added config valur
-      auth0Logout: true,
-      baseURL: APP_URL,
-   })
+ auth({
+  secret: SESSION_SECRET,
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: APP_URL,
+  // 👇 add this 👇
+  authorizationParams: {
+   response_type: "code id_token",
+   audience: "https://expenses-api",
+  },
+  // 👆 add this 👆
+ })
 );
 
 const expenses = [
@@ -79,10 +85,23 @@ app.get("/user", requiresAuth(), async (req, res) => {
 });
 
 app.get("/expenses", requiresAuth(), async (req, res, next) => {
-  res.render("expenses", {
-    user: req.oidc && req.oidc.user,
-    expenses,
+ try {
+  // 👇 get the token from the request 👇
+  const { token_type, access_token } = req.oidc.accessToken;
+  // 👇 then send it as an authorization header 👇
+  const expenses = await axios.get(`${API_URL}/reports`, {
+   headers: {
+    Authorization: `${token_type} ${access_token}`,
+   },
   });
+  // 👆 end of changes 👆
+  res.render("expenses", {
+   user: req.oidc && req.oidc.user,
+   expenses: expenses.data,
+  });
+ } catch (err) {
+  next(err);
+ }
 });
 
 // catch 404 and forward to error handler
